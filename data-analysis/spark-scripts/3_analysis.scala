24ac653eddaa358a9b85b5a3351c23a39c1c71dd
Krisna Mendoza <kbfmendoza@gmail.com>
2020-05-22 05:51:20 +0800

    Modify analysis 3

--- data-analysis/spark-scripts/3_analysis.scala
+++ data-analysis/spark-scripts/3_analysis.scala
@@ -1,10 +1,7 @@
 val spark = SparkSession.builder().appName("Spark Hive Example").config("spark.sql.warehouse.dir", warehouseLocation).enableHiveSupport().getOrCreate()
 
-val analysis2 = spark.sql("SELECT product_code as product_code, SUM(units) AS total_units FROM fact_invoice_line GROUP BY product_code ORDER BY 2 DESC LIMIT 10")
+val analysis3 = spark.sql("SELECT dim_date.cal_date, SUM(actual_unit_price*units) AS total_revenue FROM fact_invoice_line, dim_date WHERE fact_invoice_line.date_id = dim_date.id GROUP BY dim_date.cal_date")
 
-analysis2.coalesce(1).write.option("header", "true").mode("overwrite").csv("/data/retail/analysis/analysis2")
+analysis3.coalesce(1).write.option("header", "true").mode("overwrite").csv("/data/retail/analysis/analysis3")
 
-SELECT d_date.cal_date AS cal_date, SUM(actual_unit_price*units) AS total_revenue
-FROM fact_invoice f_invoice, dim_date d_date
-WHERE f_invoice.date_id = d_date.id
-GROUP BY d_date.cal_date
+SELECT dim_date.cal_date, SUM(actual_unit_price*units) AS total_revenue FROM fact_invoice_line, dim_date WHERE fact_invoice_line.date_id = dim_date.id GROUP BY dim_date.cal_date
